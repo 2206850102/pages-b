272526b1ffc35832ac5690f3b39bbf37e0a0745a
pansong291 <pansong291@sina.com>
2022-12-23 16:07:42 +0800

    build: varlet 按需导入插件

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,22 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import components from 'unplugin-vue-components/vite'
+import autoImport from 'unplugin-auto-import/vite'
+import { VarletUIResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/pages-b/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    components({
+      resolvers: [VarletUIResolver()]
+    }),
+    autoImport({
+      resolvers: [VarletUIResolver({ autoImport: true })]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
